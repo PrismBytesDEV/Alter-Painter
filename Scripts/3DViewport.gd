f491b134780a627545bb5f842857d7bb0a200731
Bartusew <103317718+Bartusew@users.noreply.github.com>
2023-08-19 22:12:41 +0200

    update4
    Huge UI overhaul
    
    added reorderable containers from addon
    added dockable containers from addon
    and smaller additions

---
+++ Scripts/3DViewport.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+@onready var mesh : MeshInstance3D = $MeshInstance3D
+
+func _ready():
+	mesh.scale = Vector3.ONE
+	if mesh.get_aabb().get_longest_axis_size() < 0.1:
+		mesh.scale = Vector3.ONE * 100
+"res://ShaderballGodot.tres"
---
+++ Scripts/CameraTrackball.gd
@@ -0,0 +1,59 @@
+@tool
+extends Marker3D
+
+var farMaxCamDistance : float
+var closeMinCamDistance : float
+
+const trackballSpeed : float = 0.01
+var trackModelMode : bool = false
+var canRotateLight : bool = false
+var rotateLightMode : bool = false
+var cameraDistance : float = 2
+@onready var Camera : Camera3D = $Camera3D
+@onready var mesh : MeshInstance3D = $"../MeshInstance3D"
+
+@onready var light : DirectionalLight3D = $"../DirectionalLight3D"
+
+
+
+func _ready():
+	recenterCamera()
+
+func _input(event):
+	if event is InputEventKey:
+		if event.keycode == KEY_SHIFT:
+			canRotateLight = false
+			if event.pressed:
+				canRotateLight = true
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			if cameraDistance > closeMinCamDistance:
+				cameraDistance -= 0.1
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			if cameraDistance < farMaxCamDistance:
+				cameraDistance += 0.1
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			trackModelMode = false
+			rotateLightMode = false
+			if event.pressed:
+				if canRotateLight:
+					rotateLightMode = true
+				else:
+					trackModelMode = true
+	
+	Camera.position = Vector3(0,0,cameraDistance)
+	
+	if event is InputEventMouseMotion:
+		if trackModelMode:
+			rotate(Vector3.UP, -event.relative.x * trackballSpeed)
+			rotate_object_local(Vector3.RIGHT, -event.relative.y * trackballSpeed)
+		if rotateLightMode:
+			light.rotate(Vector3.UP, event.relative.x * trackballSpeed)
+			light.rotate_object_local(Vector3.RIGHT, event.relative.y * trackballSpeed)
+
+func recenterCamera():
+	position = mesh.get_aabb().get_center()
+	closeMinCamDistance = mesh.get_aabb().get_longest_axis_size() * mesh.scale.x
+	farMaxCamDistance = mesh.get_aabb().get_longest_axis_size() * 4 * mesh.scale.x
+	cameraDistance = closeMinCamDistance
+	Camera.position = Vector3(0,0,cameraDistance)
