c80fb82b913ab2b021cc736d561c52434072353d
Bartusew <103317718+Bartusew@users.noreply.github.com>
2023-08-20 21:47:07 +0200

    update5
    added:
    better fill layer UI
    new tabs

---
+++ Scenes/LayerManager.gd
@@ -0,0 +1,9 @@
+extends VBoxContainer
+
+var fillLayerUI = preload("res://Scenes/layer_ui_fill.tscn") 
+@onready var layersList : ReorderableContainer = $"list scroll/List"
+
+func add_new_fill_layer():
+	var newLayer := fillLayerUI.instantiate()
+	layersList.add_child(newLayer)
+	layersList.move_child(newLayer,0)
