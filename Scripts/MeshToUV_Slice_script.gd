baee10dd1fd6f67f3c1b5306cea7d4b93c1e94dd
Bartusew <wbar13@outlook.com>
2023-10-26 22:50:01 +0200

    mesh_to_uv_slicer_progress 1

--- Scripts/MeshToUV_Slice_script.gd
+++ Scripts/MeshToUV_Slice_script.gd
@@ -1,14 +1,19 @@
 extends MeshInstance3D
 
+@export var uvVertexCoords : Array[Vector2] = []
+var vertexPositions : Array[Vector3] = []
 
 func _ready():
-	
+	uvVertexCoords.clear()
+	vertexPositions.clear()
 	var mTool : MeshDataTool = MeshDataTool.new()
 	mTool.create_from_surface(mesh,0)
 	var vertUVPos : Array[Vector2]
-	for i in mTool.get_face_count():
-		var vertI : int = 0
-		while mTool.get_face_vertex(i,vertI) != null:
-			pass
+	for faceID in mTool.get_face_count():
+		for faceVertIndex in 3:
+			var vert = mTool.get_face_vertex(faceID,faceVertIndex)
+			uvVertexCoords.append(mTool.get_vertex_uv(vert))
+			print(faceVertIndex,faceID)
+	for i in uvVertexCoords:
+		vertexPositions.append(Vector3(i.x,0,i.y))
 	
-	pass
